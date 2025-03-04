546d223ecd4504c2a346e158dd7518226e44c24e
XesusPatino <xpatinom@gmail.com>
2025-03-04 13:00:58 +0100

    Cambios Modelo GHRS

--- Features1M.py
+++ Features1M.py
@@ -1,24 +1,21 @@
 import numpy as np
 import pandas as pd
-import scipy
-import matplotlib as plt
-import networkx as nx
 import itertools
 import collections
-from sklearn.preprocessing import LabelEncoder
-from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+import networkx as nx
 
 
 def convert_categorical(df_X, _X):
     values = np.array(df_X[_X])
-    # integer encode
+    # Integer encode
     label_encoder = LabelEncoder()
     integer_encoded = label_encoder.fit_transform(values)
-    # binary encode
-    onehot_encoder = OneHotEncoder(sparse=False)
+    # Binary encode
+    onehot_encoder = OneHotEncoder(sparse_output=False)  # Cambio aquí
     integer_encoded = integer_encoded.reshape(len(integer_encoded), 1)
     onehot_encoded = onehot_encoder.fit_transform(integer_encoded)
-    df_X = df_X.drop(_X, 1)
+    df_X = df_X.drop(_X, axis=1)  # Cambio aquí
     for j in range(integer_encoded.max() + 1):
         df_X.insert(loc=j + 1, column=str(_X) + str(j + 1), value=onehot_encoded[:, j])
     return df_X
@@ -26,21 +23,21 @@ def convert_categorical(df_X, _X):
 
 dataPath = 'datasets/ml-1m/'
 
-df = pd.read_csv(dataPath+'ratings.dat', sep='\::', engine='python', names=['UID', 'MID', 'rate', 'time'])
-#dff_test = pd.read_csv(dataPath+'u1.test', sep='\\t', engine='python', names=['UID', 'MID', 'rate', 'time'])
-df_user = pd.read_csv(dataPath+'users.dat', sep='\::', engine='python', names=['UID', 'gender', 'age', 'job', 'zip'])
+# Lectura de archivos con separador corregido
+df = pd.read_csv(dataPath + 'ratings.dat', sep='::', engine='python', names=['UID', 'MID', 'rate', 'time'])
+df_user = pd.read_csv(dataPath + 'users.dat', sep='::', engine='python', names=['UID', 'gender', 'age', 'job', 'zip'])
 
-
+# Preprocesamiento de datos
 df_user = convert_categorical(df_user, 'job')
 df_user = convert_categorical(df_user, 'gender')
 df_user['bin'] = pd.cut(df_user['age'], [0, 10, 20, 30, 40, 50, 100], labels=['1', '2', '3', '4', '5', '6'])
 df_user['age'] = df_user['bin']
 
-df_user = df_user.drop('bin', 1)
+df_user = df_user.drop('bin', axis=1)  # Cambio aquí
 df_user = convert_categorical(df_user, 'age')
-df_user = df_user.drop('zip', 1)
+df_user = df_user.drop('zip', axis=1)  # Cambio aquí
 
-alpha_coefs = [0.045] #[0.005, 0.01, 0.015, 0.02, 0.025, 0.03, 0.035, 0.04, 0.045]
+alpha_coefs = [0.045]  # Puedes ajustar este valor
 
 for alpha_coef in alpha_coefs:
     pairs = []
@@ -48,7 +45,7 @@ for alpha_coef in alpha_coefs:
     for key, group in grouped:
         pairs.extend(list(itertools.combinations(group['UID'], 2)))
     counter = collections.Counter(pairs)
-    alpha = alpha_coef * 3883  # param*i_no
+    alpha = alpha_coef * 3883  # Parámetro fijo
     edge_list = map(list, collections.Counter(el for el in counter.elements() if counter[el] >= alpha).keys())
     G = nx.Graph()
 
@@ -57,25 +54,37 @@ for alpha_coef in alpha_coefs:
         G.add_edge(el[0], el[0], weight=1)
         G.add_edge(el[1], el[1], weight=1)
 
+    # Verifica que el grafo no esté vacío
+    if G.number_of_nodes() == 0 or G.number_of_edges() == 0:
+        print(f"El grafo está vacío para alpha_coef = {alpha_coef}. Ajusta el valor de alpha_coef.")
+        continue
+
+    # Cálculo de métricas de centralidad
     pr = nx.pagerank(G.to_directed())
     df_user['PR'] = df_user['UID'].map(pr)
     df_user['PR'] /= float(df_user['PR'].max())
+
     dc = nx.degree_centrality(G)
     df_user['CD'] = df_user['UID'].map(dc)
     df_user['CD'] /= float(df_user['CD'].max())
+
     cc = nx.closeness_centrality(G)
     df_user['CC'] = df_user['UID'].map(cc)
     df_user['CC'] /= float(df_user['CC'].max())
-    bc = nx.betweenness_centrality(G)
+
+    bc = nx.betweenness_centrality(G)  # No se necesita random_state
     df_user['CB'] = df_user['UID'].map(bc)
     df_user['CB'] /= float(df_user['CB'].max())
+
     lc = nx.load_centrality(G)
     df_user['LC'] = df_user['UID'].map(lc)
     df_user['LC'] /= float(df_user['LC'].max())
+
     nd = nx.average_neighbor_degree(G, weight='weight')
     df_user['AND'] = df_user['UID'].map(nd)
     df_user['AND'] /= float(df_user['AND'].max())
-    X_train = df_user[df_user.columns[1:]]
-    X_train.fillna(0, inplace=True)
 
-    X_train.to_pickle("data1m/x_train_alpha("+str(alpha_coef)+").pkl")
+    # Guardar los datos procesados
+    X_train = df_user.copy()  # Incluye todas las columnas, incluyendo 'UID'
+    X_train.fillna(0, inplace=True)
+    X_train.to_pickle(f"data1m/x_train_alpha({alpha_coef}).pkl")
---
+++ ls
@@ -0,0 +1,101 @@
+[33mcommit f5df76012a6a95a17bc6edf9ed77987eac8021c0[m[33m ([m[1;36mHEAD -> [m[1;32mmain[m[33m, [m[1;31morigin/main[m[33m, [m[1;31morigin/HEAD[m[33m)[m
+Author: Zahra Z. <25668638+zamanzadeh@users.noreply.github.com>
+Date:   Tue Nov 12 22:08:43 2024 +1100
+
+    Update README.md
+
+[33mcommit c7d58038332f391f9939bd5b06c5fa46d0407eb4[m
+Author: Hadi Valipour <hadi.valipoor@gmail.com>
+Date:   Sat Nov 12 16:41:31 2022 +1100
+
+    Update README.md
+
+[33mcommit 30ef7ffc20235e18f6e07168083bf201ef2b6696[m
+Author: Hadi Valipour <hadi.valipoor@gmail.com>
+Date:   Sun Apr 10 15:47:32 2022 +1000
+
+    Update README.md
+
+[33mcommit eb3705f6299d55ee6e36d038cb5cf4de606c1eeb[m
+Author: Hadi Valipour <hadi.valipoor@gmail.com>
+Date:   Sun Apr 10 06:48:12 2022 +1000
+
+    Delete mku.sh
+
+[33mcommit d4cdef5fd1145e1981ebe67ad828cdcf2a532ab9[m
+Author: Hadi <hadi.valipoor@gmail.com>
+Date:   Wed Mar 23 10:26:30 2022 +1100
+
+    Update README.md
+
+[33mcommit ab3a7276758a01abdad84f21dc3d0f326ccdd7d7[m
+Author: Hadi <hadi.valipoor@gmail.com>
+Date:   Wed Mar 23 10:25:46 2022 +1100
+
+    Update README.md
+
+[33mcommit 005766eee7a546beee7e2331b53364a6af6dde54[m
+Author: Hadi <hadi.valipoor@gmail.com>
+Date:   Tue Mar 22 23:48:10 2022 +1100
+
+    Update README.md
+
+[33mcommit 25653a1a06194dac86110b289e7f0bbfe5ff91c6[m
+Author: hadoov <hadi.valipoor@gmail.com>
+Date:   Tue Mar 22 23:46:15 2022 +1100
+
+    update readme file
+
+[33mcommit e332d6356200803a341746f3f9c7cb599939764e[m
+Author: hadoov <hadi.valipoor@gmail.com>
+Date:   Tue Mar 22 23:26:09 2022 +1100
+
+    Add data and figs
+
+[33mcommit 57db0504a074e8f39006211a3e31bcea32ef1d01[m
+Author: hadoov <hadi.valipoor@gmail.com>
+Date:   Tue Mar 22 23:20:01 2022 +1100
+
+    Update readme.md
+
+[33mcommit d3f0f6eaee59b1ce3945087c6394f85cd0ee431e[m
+Author: Hadi <hadi.valipoor@gmail.com>
+Date:   Tue Mar 22 23:06:58 2022 +1100
+
+    Delete 1
+
+[33mcommit 3f714aab33a7d9733f06efb4dd38b0bbfc37c738[m
+Author: Hadi <hadi.valipoor@gmail.com>
+Date:   Tue Mar 22 23:06:49 2022 +1100
+
+    Create 1
+
+[33mcommit 5d324cde904561932c3ca94e8b46306990be74c0[m
+Author: Hadi <hadi.valipoor@gmail.com>
+Date:   Tue Mar 22 23:05:33 2022 +1100
+
+    Delete fig03_architecture.png
+
+[33mcommit 22230bf90ff7d095f7a0d6bc2234bfd16503b2b0[m
+Author: Hadi <hadi.valipoor@gmail.com>
+Date:   Tue Mar 22 23:04:38 2022 +1100
+
+    Add files via upload
+
+[33mcommit 3b8264766a60cc409fcdb7b585e1f529fc4a9dc7[m
+Author: Hadi <hadi.valipoor@gmail.com>
+Date:   Tue Mar 22 22:39:49 2022 +1100
+
+    Create Features1M.py
+
+[33mcommit 4b0d8925a6eee63d97c136cfe3f16194c157fa3f[m
+Author: Hadi <hadi.valipoor@gmail.com>
+Date:   Tue Mar 22 22:39:04 2022 +1100
+
+    Create Features100K.py
+
+[33mcommit f23094ff02060f2d7891bf42d3509984e57a55e8[m
+Author: Hadi <hadi.valipoor@gmail.com>
+Date:   Tue Mar 22 18:18:39 2022 +1100
+
+    Initial commit
